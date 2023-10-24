cf45736ee1b82fd46e41db5f70efb7ea82f605ba
Alişan Karababa <alisankarababa@gmail.com>
2023-10-24 20:48:33 +0300

    added dummy data for now

--- src/store/reducers/reducerCities.js
+++ src/store/reducers/reducerCities.js
@@ -1,6 +1,11 @@
 import { CITIES_ADD, CITIES_REMOVE } from "../actions/actionsCities";
 
-const initialState = [];
+const initialState = [
+	{ name: "İstanbul", max: 15, min: 0, weather: "cloudy" },
+    { name: "Sivas", max: 7, min: -3, weather: "snowy" },
+    { name: "Bursa", max: 12, min: 4, weather: "rainy" },
+    { name: "Adana", max: 22, min: 17, weather: "sunny" },
+];
 
 export default function reducerCities(state=initialState, action) {
 
