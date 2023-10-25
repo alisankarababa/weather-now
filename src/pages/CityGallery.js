f8169a925ca7ea6795e12093c096197f4f814dd9
Alişan Karababa <alisankarababa@gmail.com>
2023-10-25 21:07:19 +0300

    changing the number of cards to show based on the width of city-gallery__cards.  circling cities with buttons

--- src/pages/CityGallery.js
+++ src/pages/CityGallery.js
@@ -6,25 +6,71 @@ import CardAddCity from "../components/CardAddCity"
 
 import iconRight from "../assets/chevron-compact-right.svg"
 import iconLeft from "../assets/chevron-compact-left.svg"
+import { useState, useEffect, useRef } from "react"
+
+import { arrSliceCircular, arrIncrementIdxCircularly, arrDecrementIdxCircularly } from "../utils/utils"
 
 export default function CityGallery() {
     
     const cities = useSelector((store) => store.reducerCities);
+    const [idxStart, setIdxStart] = useState(0);
+    const [citiesToShow, setCitiesToShow] = useState([]);
+    const [cntCityCardToShow, setCntCityCardToShow] = useState(0);
 
+    const cityGalleryCards = useRef(null);
+    const [widthCityGalleryCards, setWidthCityGalleryCards] = useState(0);
+    
+    useEffect(() => {
 
-    console.log(cities);
-    return (
+        if(widthCityGalleryCards >= 1000) {
+            setCntCityCardToShow(4);
+        }
+        else if(widthCityGalleryCards >= 800) {
+            setCntCityCardToShow(3);
 
-        <div className="city-gallery">
-            <img className="city-gallery__button"src={iconLeft} alt="icon-left"/>
-            {
-                cities.map(city => <CityCard className="city-gallery__item" key={city.id} city={city}/>)
+        } else if(widthCityGalleryCards >= 650) {
+            setCntCityCardToShow(2);
+        }
+        else {
+            setCntCityCardToShow(1);
+        }
+    }, [widthCityGalleryCards])
+
+    useEffect(() => {
+        setCitiesToShow(arrSliceCircular(cities, idxStart, cntCityCardToShow));
+    }, [idxStart, cities, cntCityCardToShow])
+
+    useEffect(() => {
+        const updateComponentWidth = () => {
+            if (cityGalleryCards.current) {
+            const width = cityGalleryCards.current.getBoundingClientRect().width;
+            setWidthCityGalleryCards(width);
             }
-            <CardAddCity className="city-gallery__item"/>
-            <img className="city-gallery__button" src={iconRight} alt="icon-rigth"/>
+        };
+    
+        updateComponentWidth();
+    
+        window.addEventListener('resize', updateComponentWidth);
+    
+        return () => {
+            window.removeEventListener('resize', updateComponentWidth);
+        };
+    }, []);
+
+    return (
+        <>
+        <div className="city-gallery">
+            <img onClick={() => setIdxStart(arrDecrementIdxCircularly(cities, idxStart))} className="city-gallery__button"src={iconLeft} alt="icon-left"/>
+            <div ref={cityGalleryCards} className="city-gallery__cards">
+                {
+                    citiesToShow.map(city => <CityCard className="city-gallery__card" key={city.id} city={city}/>)
+                }
+                { widthCityGalleryCards >= 320 && <CardAddCity className="city-gallery__card"/> }
+            </div>
+            <img onClick={() => setIdxStart(arrIncrementIdxCircularly(cities, idxStart))} className="city-gallery__button" src={iconRight} alt="icon-rigth"/>
         </div>
+        { widthCityGalleryCards < 320 && <button className="btn btn-pill bg-purple">ADD CITY</button>}
+        </>
+
     );
-
-
-
 }
