8fe3deb90c10aa46e69a9cd6cd33bd17bc04f2c6
Alişan <alisankarababa@gmail.com>
2023-11-13 20:48:59 +0300

    update header text

--- src/layout/Header.js
+++ src/layout/Header.js
@@ -6,7 +6,7 @@ export default function Header() {
     return (
         <header className="header ">
             <div className="container">
-                Weather App
+                Weather Now
             </div>
         </header>
     )
