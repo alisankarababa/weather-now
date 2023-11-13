2de75490498b85a1b6c780bba170b0b8d206fb30
Alişan Karababa <alisankarababa@gmail.com>
2023-11-13 14:45:47 +0300

    add header to show app name

---
+++ src/layout/Header.js
@@ -0,0 +1,13 @@
+import "./Header.css"
+
+
+export default function Header() {
+
+    return (
+        <header className="header ">
+            <div className="container">
+                Weather App
+            </div>
+        </header>
+    )
+}
